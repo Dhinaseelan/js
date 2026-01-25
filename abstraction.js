f0137c967f3dc06ccd8a69bdaa12578250331cad
Dhinaseelan <dhinaseeland@gmail.com>
2026-01-25 15:14:03 +0530

    feat: Static Properties and method

--- abstraction.js
+++ abstraction.js
@@ -34,3 +34,40 @@ console.log(godrive.getstatus())
 godrive.drive(20)
 godrive.refuel(30)
 godrive.refuel(80)
+class address{
+    static addres="mgr nagar";
+    static pin=605104;
+    static addresp(){
+        return `my address is ${this.addres+this.pin}`
+    }
+}
+console.log(address.addresp())
+class shoppingitem {
+    static totalitem = 0;
+    static item_list = [];
+
+    constructor(name, amount) {
+        this.name = name;
+        this.amount = amount;
+        this.prolist = { name: this.name, amount: this.amount }; // FIXED
+        this.constructor.totalitem++;
+        this.constructor.item_list.push(this.prolist);
+    }
+
+    static getTotalitems() {
+        return this.totalitem;
+    }
+
+    static getprolist() {
+        return this.item_list.map((item, index) => {
+            return `product ${index + 1} product name:${item.name} product price:${item.amount}`;
+        });
+    }
+}
+
+const pro1 = new shoppingitem("charger", 2599);
+const pro2 = new shoppingitem("laptop", 33599);
+const pro3 = new shoppingitem("pen", 23599);
+
+console.log(shoppingitem.getTotalitems()); 
+console.log(shoppingitem.getprolist());
